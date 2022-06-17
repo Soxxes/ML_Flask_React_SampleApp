3982cc3aa58d47835f38b1be9953803c989d4806
Soxxes <hscschwimmermarc@t-online.de>
2022-06-17 15:15:06 +0200

    added: model prediction

---
+++ backend/rest/model.py
@@ -0,0 +1,16 @@
+from flask import request
+from flask_restful import Resource
+import numpy as np
+
+from rest._common import get_response
+from logic.linear_model import predict
+
+class LinearModelApi(Resource):
+
+    def post(self):
+        x = int(request.get_json().get("X"))
+        x = np.array(x)
+        result = predict(x)
+        return get_response({
+            "prediction": result[0]
+        })
